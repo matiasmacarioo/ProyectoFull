d535e3a442791d6aeab322f3051d3fe614856de6
matiasmacarioo <matiasmacarioo@gmail.com>
2023-11-02 19:24:41 -0300

    Filtrar tareas por carrera del alumno

--- Controllers/TareaController.cs
+++ Controllers/TareaController.cs
@@ -6,23 +6,40 @@ using System.Threading.Tasks;
 using Proyecto.Models;
 using Proyecto.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Proyecto.Controllers
 {
- 
- [Authorize]
+
+    [Authorize]
     public class TareasController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _rolManager;
 
-        public TareasController(ApplicationDbContext context)
+        public TareasController(ApplicationDbContext context, ApplicationDbContext contextUsuario, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> rolManager)
         {
             _context = context;
+            _userManager = userManager;
+            _rolManager = rolManager;
         }
 
         public async Task<IActionResult> Index()
         {
             var tareas = _context.Tareas?.Include(a => a.Asignatura).OrderByDescending(a => a.Fecha).ToList();
+            var usuario = await _userManager.GetUserAsync(User);
+
+            if (usuario != null)
+            {
+                // Encontrar alumno por su campo IdentityID igual al id del usuario identity.
+                var alumno = _context.Alumnos.Where(a => a.IdentityID == usuario.Id).FirstOrDefault();
+                // Cuscar tareas de la carrera del alumno que inició sesión.
+                if (alumno != null)
+                {
+                    tareas =  _context.Tareas?.Include(a => a.Asignatura).OrderByDescending(a => a.Fecha).Where(t => t.Asignatura.CarreraID == alumno.CarreraID).ToList();
+                }
+            }
             var asignaturas = _context.Asignaturas?.ToList();
             ViewBag.AsignaturasList = new SelectList(asignaturas, "AsignaturaID", "Nombre");
             ViewBag.UsuariosList = new SelectList(_context.Users, "Id", "Email");
